a3d0d520de6edc42faa0188f7bd3a1cedba7dafb
Igor Shapilov <i.shapilov@ispsystem.com>
2021-02-07 15:06:25 +0800

    chore(configs-i18n): updated configs, added i18n

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderModule } from './components/header/header.module';
+import { MainModule } from './main/main.module';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+/** Http loader factory */
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HeaderModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HeaderModule,
+    MainModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+      defaultLanguage: 'ru',
+    }),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
